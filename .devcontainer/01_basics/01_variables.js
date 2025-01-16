dfd84ed65f72cb16e1a48c5178d43bcbdff3e2f4
DebufreeGit <debabratasahu778@gmail.com>
2025-01-16 05:08:55 +0000

    config day1

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 144553
+let accountEamil = "debusahu665@gmail.com"
+var accountpassword = "12345"
+accountCity = "Odisha"
+accountEamil = "dfshfj@hjhj"
+accountpassword = "hbhbh"
+accountCity = "india"
+let accountState = "Orrisa"
+//accountId = 3223 not allowed
+console.log(accountId);
+console.table([accountEamil,accountId,accountpassword,accountCity,accountState])
+
+
+/*
+Prefer not to use var 
+because of issue in block scope and functioanl scope
+*/
---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all the javascript code as newer version
+//alart(3+3)// we are usnig Nodejs,not browser
+// code reasability should be high 
+
+let name = "Debu" //string ""
+let age =29  // number =>2 to power 53 //bigint
+let goodboy = false //boolean =>True/false
+
+//null =>its a standalon value
+//undefind => unassigned value
+//symbol => unique 
+
+
+//object = null is object
+console.log(typeof "Debu") // string
+console .log(typeof null) // object
+console.log(typeof undefined)//undefind
+
